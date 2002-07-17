579fd2bfcff25b93f889c4dcfc4b5aa43e4b755e
Michael Andres <ma@suse.de>
2002-07-17 20:25:40 +0000

    - UniqStr substituted by Ustring.
    svn path=/trunk/liby2util/; revision=3718

---
+++ src/include/y2util/Ustring.h
@@ -0,0 +1,175 @@
+/*---------------------------------------------------------------------\
+|                                                                      |
+|                      __   __    ____ _____ ____                      |
+|                      \ \ / /_ _/ ___|_   _|___ \                     |
+|                       \ V / _` \___ \ | |   __) |                    |
+|                        | | (_| |___) || |  / __/                     |
+|                        |_|\__,_|____/ |_| |_____|                    |
+|                                                                      |
+|                               core system                            |
+|                                                        (C) SuSE GmbH |
+\----------------------------------------------------------------------/
+
+   File:       Ustring.h
+
+   Author:     Michael Andres <ma@suse.de>
+   Maintainer: Michael Andres <ma@suse.de>
+
+/-*/
+#ifndef Ustring_h
+#define Ustring_h
+
+#include <iostream>
+#include <string>
+
+#include <y2util/hash.h>
+
+///////////////////////////////////////////////////////////////////
+//
+//	CLASS NAME : UstringHash
+/**
+ * @short A Hash of unique std::strings.
+ *
+ * A Hash of unique std::strings.
+ *
+ * @see Ustring
+ **/
+class UstringHash {
+
+  protected:
+
+    ///////////////////////////////////////////////////////////////////
+    //
+    //	CLASS NAME : UstringHash_el
+    /**
+     * @short Content type of the UstringHash hash.
+     *
+     * Content type of the UstringHash hash. Comparinson is done by
+     * comparing the stored std::string. That way existing strings
+     * can be found by hash functions.
+     *
+     * @see UstringHash
+     **/
+    struct UstringHash_el {
+
+	const std::string _str;
+
+	explicit UstringHash_el( const std::string & str_r ) : _str( str_r ) {}
+
+        bool operator == ( const UstringHash_el & rhs ) const { return( _str == rhs._str ); }
+        bool operator != ( const UstringHash_el & rhs ) const { return( _str != rhs._str ); }
+    };
+
+    /**
+     * Hash function for UstringHash_el
+     **/
+    friend size_t hashfun( const UstringHash_el & ustr_r ){
+      return hashfun( ustr_r._str.c_str() );
+    }
+    //
+    ///////////////////////////////////////////////////////////////////
+
+    typedef noval_hash<UstringHash_el> UstringHash_type;
+
+    /**
+     * The hash of UstringHash_el
+     **/
+    UstringHash_type _UstringHash;
+
+  public:
+
+    /**
+     * Store the passed in the hash, if it does not already contain a string
+     * with the same value. Return the unique string from the hash.
+     **/
+    const std::string & add( const std::string & nstr_r ) {
+      UstringHash_type::list_type * el = _UstringHash.insert( UstringHash_el( nstr_r ) );
+      return el->key._str;
+    }
+
+    /**
+     * Return the number of unique strings stored in the hash.
+     **/
+    unsigned size() const { return _UstringHash.size(); }
+};
+
+///////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////
+//
+//	CLASS NAME : Ustring
+/**
+ * @short Base class for unique std::strings
+ *
+ * The std::string stored inside Ustring shares his representaion
+ * class (which contains the actual data) with a unique std::string
+ * stored inside a UstringHash.
+ *
+ * That way Ustrings of the same value can be stored at various locations,
+ * while the actual data are stored only once.
+ *
+ * <PRE>
+ * class PkgName : public Ustring {
+ *   private:
+ *     static UstringHash _nameHash;
+ *   public:
+ *     explicit PkgName( const std::string & n = "" ) : Ustring( _nameHash, n ) {}
+ * };
+ * </PRE>
+ *
+ * @see UstringHash
+ **/
+class Ustring {
+
+  private:
+
+    std::string _name;
+
+  public:
+
+    Ustring( UstringHash & nameHash_r, const std::string & n  )
+      :_name( nameHash_r.add( n ) )
+    {}
+
+  public:
+
+    operator const std::string & () const { return _name; }
+
+    const std::string * operator->() const { return & _name; }
+
+    friend bool operator==( const Ustring & lhs, const Ustring & rhs ) {
+      return ( (std::string)lhs == (std::string)rhs );
+    }
+
+    friend bool operator==( const Ustring & lhs, const std::string & rhs ) {
+      return ( (std::string)lhs == rhs );
+    }
+
+    friend bool operator==( const std::string & lhs, const Ustring & rhs ) {
+      return ( lhs == (std::string)rhs );
+    }
+
+    friend bool operator!=( const Ustring & lhs, const Ustring & rhs ) {
+      return ( (std::string)lhs != (std::string)rhs );
+    }
+
+    friend bool operator!=( const Ustring & lhs, const std::string & rhs ) {
+      return ( (std::string)lhs != rhs );
+    }
+
+    friend bool operator!=( const std::string & lhs, const Ustring & rhs ) {
+      return ( lhs != (std::string)rhs );
+    }
+
+    friend bool operator<( const Ustring & lhs, const Ustring & rhs ) {
+      return ( (std::string)lhs < (std::string)rhs );
+    }
+
+    friend std::ostream & operator<<( std::ostream & str, const Ustring & obj ) {
+      return str << (std::string)obj;
+    }
+};
+
+///////////////////////////////////////////////////////////////////
+
+#endif // Ustring_h
