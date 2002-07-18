c4740858a8971da4e993102972b9d8610019f5e1
Michael Andres <ma@suse.de>
2002-07-18 09:50:13 +0000

    Ustring: missing comparison operators added. Documentation added. Ready.
    svn path=/trunk/liby2util/; revision=3723

--- src/include/y2util/Ustring.h
+++ src/include/y2util/Ustring.h
@@ -28,9 +28,9 @@
 //
 //	CLASS NAME : UstringHash
 /**
- * @short A Hash of unique std::strings.
+ * @short A Hash of unique strings.
  *
- * A Hash of unique std::strings.
+ * A Hash of unique strings.
  *
  * @see Ustring
  **/
@@ -45,8 +45,8 @@ class UstringHash {
      * @short Content type of the UstringHash hash.
      *
      * Content type of the UstringHash hash. Comparinson is done by
-     * comparing the stored std::string. That way existing strings
-     * can be found by hash functions.
+     * comparing strings. That way existing strings can be found
+     * by hash functions.
      *
      * @see UstringHash
      **/
@@ -79,8 +79,9 @@ class UstringHash {
   public:
 
     /**
-     * Store the passed in the hash, if it does not already contain a string
-     * with the same value. Return the unique string from the hash.
+     * Store the passed std::string in the hash, if it does not already
+     * contain a string with the same value. Return the unique string from
+     * the hash.
      **/
     const std::string & add( const std::string & nstr_r ) {
       UstringHash_type::list_type * el = _UstringHash.insert( UstringHash_el( nstr_r ) );
@@ -99,14 +100,35 @@ class UstringHash {
 //
 //	CLASS NAME : Ustring
 /**
- * @short Base class for unique std::strings
+ * @short Unique strings
  *
- * The std::string stored inside Ustring shares his representaion
- * class (which contains the actual data) with a unique std::string
- * stored inside a UstringHash.
+ * Ustring provides an immutable string and uses a UstringHash
+ * to keep the strings representaion class (which contains the
+ * actual data) unique.
  *
- * That way Ustrings of the same value can be stored at various locations,
- * while the actual data are stored only once.
+ * That way Ustrings of the same value and unsing the same
+ * UstringHash can be stored at various locations, while the
+ * actual string data are stored only once.
+ *
+ * The UstringHash to use is passed to the constructor, and no
+ * more needed after the string has been stored.
+ *
+ * Conversion to string is possible and cheap, as the created
+ * string will, unless he's modified, share it's data with the
+ * one inside the Ustring.
+ *
+ * Comparison between Ustrings and strings are based on string.
+ *
+ * A <CODE>-></CODE> operator is provided as an easy way to
+ * invoke const string methods, like size() or c_str().
+ *
+ * <PRE>
+ *   ustr->size(); // short for ((const std::string &)u).size();
+ * </PRE>
+ *
+ * Most common usage will be deriving somne class from Ustring,
+ * that provides a static UstringHash, and some constructor.
+ * Everything else is provided by Ustring.
  *
  * <PRE>
  * class PkgName : public Ustring {
@@ -123,50 +145,122 @@ class Ustring {
 
   private:
 
+    /**
+     * !!! It should actualy be const !!!
+     * But that way default copy and assingment can be used.
+     **/
     std::string _name;
 
   public:
 
+    /**
+     * Constructor calls @ref UstringHash::add on the given string,
+     * and stores the string returned from the hash.
+     **/
     Ustring( UstringHash & nameHash_r, const std::string & n  )
       :_name( nameHash_r.add( n ) )
     {}
 
   public:
 
+    /**
+     * Conversion to const std::string &
+     **/
     operator const std::string & () const { return _name; }
 
+    /**
+     * ustr->size(); // short for ((const std::string &)ustr).size();
+     **/
     const std::string * operator->() const { return & _name; }
 
+    // operator ==
+
     friend bool operator==( const Ustring & lhs, const Ustring & rhs ) {
-      return ( (std::string)lhs == (std::string)rhs );
+      return ( (const std::string &)lhs == (const std::string &)rhs );
     }
 
     friend bool operator==( const Ustring & lhs, const std::string & rhs ) {
-      return ( (std::string)lhs == rhs );
+      return ( (const std::string &)lhs == rhs );
     }
 
     friend bool operator==( const std::string & lhs, const Ustring & rhs ) {
-      return ( lhs == (std::string)rhs );
+      return ( lhs == (const std::string &)rhs );
     }
 
+    // operator !=
+
     friend bool operator!=( const Ustring & lhs, const Ustring & rhs ) {
-      return ( (std::string)lhs != (std::string)rhs );
+      return ( ! operator==( lhs, rhs ) );
     }
 
     friend bool operator!=( const Ustring & lhs, const std::string & rhs ) {
-      return ( (std::string)lhs != rhs );
+      return ( ! operator==( lhs, rhs ) );
     }
 
     friend bool operator!=( const std::string & lhs, const Ustring & rhs ) {
-      return ( lhs != (std::string)rhs );
+      return ( ! operator==( lhs, rhs ) );
     }
+
+    // operator <
 
     friend bool operator<( const Ustring & lhs, const Ustring & rhs ) {
-      return ( (std::string)lhs < (std::string)rhs );
+      return ( (const std::string &)lhs < (const std::string &)rhs );
     }
 
+    friend bool operator<( const Ustring & lhs, const std::string & rhs ) {
+      return ( (const std::string &)lhs < rhs );
+    }
+
+    friend bool operator<( const std::string & lhs, const Ustring & rhs ) {
+      return ( lhs < (const std::string &)rhs );
+    }
+
+    // operator >
+
+    friend bool operator>( const Ustring & lhs, const Ustring & rhs ) {
+      return ( (const std::string &)lhs > (const std::string &)rhs );
+    }
+
+    friend bool operator>( const Ustring & lhs, const std::string & rhs ) {
+      return ( (const std::string &)lhs > rhs );
+    }
+
+    friend bool operator>( const std::string & lhs, const Ustring & rhs ) {
+      return ( lhs > (const std::string &)rhs );
+    }
+
+    // operator >=
+
+    friend bool operator>=( const Ustring & lhs, const Ustring & rhs ) {
+      return ( ! operator<( lhs, rhs ) );
+    }
+
+    friend bool operator>=( const Ustring & lhs, const std::string & rhs ) {
+      return ( ! operator<( lhs, rhs ) );
+    }
+
+    friend bool operator>=( const std::string & lhs, const Ustring & rhs ) {
+      return ( ! operator<( lhs, rhs ) );
+    }
+
+    // operator <=
+
+    friend bool operator<=( const Ustring & lhs, const Ustring & rhs ) {
+      return ( ! operator>( lhs, rhs ) );
+    }
+
+    friend bool operator<=( const Ustring & lhs, const std::string & rhs ) {
+      return ( ! operator>( lhs, rhs ) );
+    }
+
+    friend bool operator<=( const std::string & lhs, const Ustring & rhs ) {
+      return ( ! operator>( lhs, rhs ) );
+    }
+
+    // IO
+
     friend std::ostream & operator<<( std::ostream & str, const Ustring & obj ) {
-      return str << (std::string)obj;
+      return str << (const std::string &)obj;
     }
 };
 
