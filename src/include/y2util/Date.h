9b3676ae97cf820d69635aedff0079b0ee18a87b
Michael Andres <ma@suse.de>
2002-08-13 16:37:12 +0000

    Class Date: Store and operate on date (time_t) values. Currently supports - free conversion to string using a 'strftime' compatible format string - conversion from/to string representation of calendar time in numeric form (like "1029255142") - conversion to a default string representation for the current locale.
    svn path=/trunk/liby2util/; revision=4629

--- src/include/y2util/Date.h
+++ src/include/y2util/Date.h
@@ -36,15 +36,69 @@ class Date {
 
   private:
 
+    /**
+     * Calendar time. The number of seconds elapsed since
+     * 00:00:00 on January 1, 1970, Coordinated Universal Time (UTC).
+     **/
     time_t _date;
 
   public:
 
-    Date( time_t date_r = 0 ) {
-      _date = date_r;
-    }
+    // static constructors and conversion
 
+    /**
+     * Return the current time.
+     **/
+    static time_t now() { return time( 0 ); }
+
+    /**
+     * Return string representation of date according to format.
+     * See 'man strftime' (which is used internaly) for valid
+     * conversion specifiers in format.
+     *
+     * Retruns an empty string on illegal format.
+     **/
+    static std::string form( const std::string & format, time_t tval_r );
+
+    /**
+     * Convert from string representation of calendar time in
+     * numeric form (like "1029255142").
+     **/
+    static time_t fromSECONDS( const std::string & str_r );
+
+    /**
+     * Convert to string representation of calendar time in
+     * numeric form (like "1029255142").
+     **/
+    static std::string toSECONDS( time_t tval_r );
+
+  public:
+
+    /**
+     * Constructor
+     **/
+    Date( time_t date_r = 0 ) : _date( date_r ) {}
+
+    /**
+     * Conversion to time_t
+     **/
     operator time_t() const { return _date; }
+
+    /**
+     * Member version of 'static form'.
+     **/
+    std::string form( const std::string & format ) const { return form( format, _date ); }
+
+    /**
+     * Default string representation of Date. The preferred
+     * date and time representation for the current locale.
+     **/
+    std::string asString() const;
+
+    /**
+     * Write asString.
+     **/
+    friend std::ostream & operator<<( std::ostream & str, const Date & obj );
 };
 
 ///////////////////////////////////////////////////////////////////
