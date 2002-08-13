4b469ecf0777b7a9ef23f583a89cce548046809a
Michael Andres <ma@suse.de>
2002-08-13 12:22:01 +0000

    Classes Date(time_t)/FSize(long long) reduced to minimum. Going to overwork the old YaST interface.
    svn path=/trunk/liby2util/; revision=4614

--- src/include/y2util/Date.h
+++ src/include/y2util/Date.h
@@ -1,79 +1,52 @@
-/* $Header$
-//
-// Date.h
-//
-// Helper class for date related data
-//
-// AUTHOR	Michael Andres <ma@suse.de>
-// MAINTAINER	Michael Andres <ma@suse.de>
-//
-// Based on Revision 10.14.1.3  2001/08/27 14:44:12 from YaST_SuSE_Linux_7_3/WidLib/../PkgLib/RCS/Date.h
-//
-*/
+/*---------------------------------------------------------------------\
+|                                                                      |
+|                      __   __    ____ _____ ____                      |
+|                      \ \ / /_ _/ ___|_   _|___ \                     |
+|                       \ V / _` \___ \ | |   __) |                    |
+|                        | | (_| |___) || |  / __/                     |
+|                        |_|\__,_|____/ |_| |_____|                    |
+|                                                                      |
+|                               core system                            |
+|                                                        (C) SuSE GmbH |
+\----------------------------------------------------------------------/
 
+  File:       Date.h
+
+  Author:     Michael Andres <ma@suse.de>
+  Maintainer: Michael Andres <ma@suse.de>
+
+  Purpose: Store and operate on date (time_t).
+
+/-*/
 #ifndef _Date_h_
 #define _Date_h_
 
-extern "C"
-{
-#include <time.h>
-}
+#include <ctime>
+#include <iosfwd>
 #include <string>
 
 ///////////////////////////////////////////////////////////////////
 //
 //	CLASS NAME : Date
 //
-//	DESCRIPTION :
-//
+/**
+ * @short Store and operate on date (time_t).
+ **/
 class Date {
-
-  friend ostream & operator<<( ostream & outs, const Date & obj ) {
-    return outs << obj.Asstd::string();
-  }
 
   private:
 
-    time_t Date_i;
+    time_t _date;
 
   public:
 
-    /**
-     * "YYYYMMDDhhmm" for construct and coding
-     */
-    Date( time_t i = 0 )          { Date_i = i; }
-    Date( const std::string & date_t );
-
-    static Date   Decode( const std::string & date_tv );
-    static std::string Encode( const Date &   date_iv );
-
-    operator time_t() const { return Date_i; }
-
-    static const time_t Second_i;
-    static const time_t Minute_i;
-    static const time_t Hour_i;
-    static const time_t Day_i;
-    static const time_t HalfYear_i;
-    static const time_t Year_i;
-
-    static time_t Now() { return time( 0 ); }
-
-    /**
-     * "Wed Jan 13 17:45:41 1999"
-     */
-    std::string   AsString() const {
-      std::string Ret( ctime( &Date_i ) );
-      Ret.at( "\n" ) = "";
-      return Ret;
+    Date( time_t date_r = 0 ) {
+      _date = date_r;
     }
 
-    /**
-     *  "Feb  3 00:18" for last half year
-     *  "Feb  3  1999" else
-     */
-    std::string ShortDate() const;
+    operator time_t() const { return _date; }
 };
 
 ///////////////////////////////////////////////////////////////////
 
-#endif
+#endif // _Date_h_
--- src/include/y2util/FSize.h
+++ src/include/y2util/FSize.h
@@ -1,93 +1,55 @@
-/* $Header$
-//
-// FSize.h
-//
-// Helper class for file size related data
-//
-// AUTHOR	Michael Andres <ma@suse.de>
-// MAINTAINER	Michael Andres <ma@suse.de>
-//
-// Based on Revision 10.14.1.2  2001/08/27 14:44:12 from YaST_SuSE_Linux_7_3/PkgLib/RCS/FSize.h
-//
-*/
+/*---------------------------------------------------------------------\
+|                                                                      |
+|                      __   __    ____ _____ ____                      |
+|                      \ \ / /_ _/ ___|_   _|___ \                     |
+|                       \ V / _` \___ \ | |   __) |                    |
+|                        | | (_| |___) || |  / __/                     |
+|                        |_|\__,_|____/ |_| |_____|                    |
+|                                                                      |
+|                               core system                            |
+|                                                        (C) SuSE GmbH |
+\----------------------------------------------------------------------/
 
+  File:       FSize.h
+
+  Author:     Michael Andres <ma@suse.de>
+  Maintainer: Michael Andres <ma@suse.de>
+
+  Purpose: Store and operate on (file/package/partition) sizes.
+
+/-*/
 #ifndef _FSize_h_
 #define _FSize_h_
 
+#include <iosfwd>
 #include <string>
-#include <iostream>
-
 
 ///////////////////////////////////////////////////////////////////
 //
 //	CLASS NAME : FSize
 //
-//	DESCRIPTION : Store and operate on (file/package/partition)
-//                    sizes. Use Str(...) to create various std::string
-//                    representaitons of FSize.
-//
+/**
+ * @short Store and operate on (file/package/partition) sizes.
+ **/
 class FSize {
 
   public:
 
-    enum UNIT { B = 0, K, M, G };
-
-    friend istream & operator>>( istream & STREAM, FSize & OBJ );
-    friend ostream & operator<<( ostream & STREAM, const FSize & OBJ );
-    friend ostream & operator<<( ostream & STREAM, const FSize::UNIT & OBJ );
-
-    static const double & Fac( const UNIT u_bv = B ) {
-      static const double fac[4] = { 1, 1024, 1024*1024, 1024*1024*1024 };
-      return fac[u_bv];
-    }
-
-    static const char * Unit( const UNIT u_bv = B ) {
-      static const char * unit[4] = { "B", "K", "M", "G" };
-      return unit[u_bv];
-    }
+    enum Unit { B = 0, K, M, G, T };
 
   private:
 
-    double Size_f;
+    long long _size;
 
   public:
 
-    FSize( const double Size_fv = 0.0, const UNIT u_bv = B ) { Set( Size_fv, u_bv ); }
-    ~FSize(){}
+    FSize( long long size_r, const Unit unit_r = B  ) {
+      _size = size_r; // preliminarily
+    }
 
-  public:
-
-    operator double() const { return Size_f; }
-
-    FSize operator-() { return -Size_f ; }
-
-    FSize operator+( const FSize & Rhs ) { return Size_f + Rhs.Size_f; }
-    FSize operator-( const FSize & Rhs ) { return Size_f - Rhs.Size_f; }
-    FSize operator*( const FSize & Rhs ) { return Size_f * Rhs.Size_f; }
-    FSize operator/( const FSize & Rhs ) { return Size_f / Rhs.Size_f; }
-
-    FSize & operator= ( const FSize & Rhs ) { Size_f  = Rhs.Size_f; return *this; }
-    FSize & operator+=( const FSize & Rhs ) { Size_f += Rhs.Size_f; return *this; }
-    FSize & operator-=( const FSize & Rhs ) { Size_f -= Rhs.Size_f; return *this; }
-    FSize & operator*=( const FSize & Rhs ) { Size_f *= Rhs.Size_f; return *this; }
-    FSize & operator/=( const FSize & Rhs ) { Size_f /= Rhs.Size_f; return *this; }
-
-
-    FSize & Set( const double Size_fv, const UNIT u_bv = B ) { Size_f = Size_fv * Fac( u_bv ); return *this; }
-    double  operator()( const UNIT u_bv = B ) const          { return Size_f  / Fac( u_bv ); }
-
-  public:
-
-    UNIT   Best() const;
-
-    static int DefStrLen( const bool ShowUnit_bv = true ) { return ShowUnit_bv ? 7 : 5; }
-
-    std::string Str( const UNIT u_bv, int Fw_iv, const bool ShowUnit_bv = true ) const;
-    std::string Str( const UNIT u_bv, const bool ShowUnit_bv = true ) const { return Str(   u_bv, DefStrLen( ShowUnit_bv ), ShowUnit_bv ); }
-    std::string Str( int Fw_iv, const bool ShowUnit_bv = true )       const { return Str( Best(),                    Fw_iv, ShowUnit_bv ); }
-    std::string Str( const bool ShowUnit_bv = true )                  const { return Str( Best(), DefStrLen( ShowUnit_bv ), ShowUnit_bv ); }
+    operator long long() const { return _size; }
 };
 
 ///////////////////////////////////////////////////////////////////
 
-#endif _FSize_h_
+#endif // _FSize_h_
