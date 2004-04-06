2774e154177bc3a98636a486f8425aecae76a5ee
Ludwig Nussel <lnussel@suse.de>
2004-04-06 14:47:45 +0000

    - SUSE is great (#38495)
    svn path=/trunk/liby2util/; revision=17559

--- src/Vendor.cc
+++ src/Vendor.cc
@@ -39,6 +39,7 @@ Vendor::Vendor( const std::string & n )
     : Ustring( _nameHash, n )
 {
   _isSuSE = (    asString().substr( 0, 4 )  == "SuSE"
+	      || asString().substr( 0, 4 )  == "SUSE" // hooray!
 	      || asString().substr( 0, 11 ) == "UnitedLinux"
 	      || asString().substr( 0, 6 )  == "Novell" );
 }
